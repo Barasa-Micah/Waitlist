57d316a2519d4beda5588923ab01d17db2f21906
Vee-Kathure <victoriakathure@gmail.com>
2024-06-22 18:19:05 +0300

    added view password icon

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useForm } from 'react-hook-form';
 import ErrorSharpIcon from '@mui/icons-material/ErrorSharp';
 import { pink } from '@mui/material/colors';
+import { AiFillEye } from "react-icons/ai";
+import { AiFillEyeInvisible } from "react-icons/ai";
+
 
 const SignUpForm = () => {
     const { handleSubmit,
@@ -26,7 +29,11 @@ console.log(errors, {isSubmitted} )
         console.log(data);
     };
     
-    
+    const [open, setOpen] = useState(false)
+    // handle toggle
+    const toggle = () => {
+      setOpen(!open)
+    }
 
     return (
        <>
@@ -35,6 +42,7 @@ console.log(errors, {isSubmitted} )
              
           <form onSubmit={handleSubmit(onSignup)} noValidate
           className='space-y-4 md:space-y-6 max-w-[400px] w-full bg-white '>
+            <p className='text-center text-lg font-sans font-medium'>Sokobeauty Registration</p>
              <div>
                 <label className='block mb-2 text-sm font-medium text-gray-900'>First Name:</label>
                 <input type="text" id='firstname' {...register("firstName", {
@@ -63,7 +71,7 @@ console.log(errors, {isSubmitted} )
                        message: 'Last name is required',
                     }})}
                 className='border border-gray-400 focus:shadow-md focus:border-amber-500 focus:outline-none focus:ring-0 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5' placeholder="lastname"  />
-                <p className='text-xs font-medium text-pink-600 text-left mb-2'>
+                <p className='text-sm font-medium text-pink-600 text-left mt-1 mb-2'>
                     {errors.lastName?.message && (
     <span className='flex items-center'>
      <ErrorSharpIcon className='mr-1' sx={{ color: pink[500], fontSize: 16, verticalAlign: 'middle' }} />
@@ -85,7 +93,7 @@ console.log(errors, {isSubmitted} )
                     }
                 })}
                  className=' border border-gray-400 focus:shadow-md focus:border-amber-500 focus:outline-none focus:ring-0 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5' placeholder="your email"  />
-                <p className='text-xs font-medium text-pink-600 text-left mb-2'>{errors.email?.message && (
+                <p className='text-sm font-medium text-pink-600 text-left mt-1 mb-2'>{errors.email?.message && (
     <span className='flex items-center'>
      <ErrorSharpIcon className='mr-1' sx={{ color: pink[500], fontSize: 16, verticalAlign: 'middle' }} />
      {errors.email.message}
@@ -100,27 +108,33 @@ console.log(errors, {isSubmitted} )
                        message: 'Username is required',
                     }})}
                 className=' border border-gray-400 focus:shadow-md focus:border-amber-500 focus:outline-none focus:ring-0 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5' placeholder="username"  />
-                <p className='text-xs font-medium text-pink-600 text-left mb-2'>{errors.username?.message && (
+                <p className='text-sm font-medium text-pink-600 text-left mt-1 mb-2'>{errors.username?.message && (
     <span className='flex items-center'>
      <ErrorSharpIcon className='mr-1' sx={{ color: pink[500], fontSize: 16, verticalAlign: 'middle' }} />
      {errors.username.message}
     </span>
   )}</p>
              </div>
-             <div>
+             <div className='relative'>
                 <label className='block mb-2 text-sm font-medium text-gray-900 '>Password:</label>
-                <input type="password" id='password' {...register("password", {
+                <input type={(open === false)? 'password'  : 'text'} id='password' {...register("password", {
                     required: {
                        value: true,
                        message: 'Password is required',
                     }})}
                 className=' border border-gray-400 focus:shadow-md focus:border-amber-500 focus:outline-none focus:ring-0 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5' placeholder=""/>
-                <p className='text-xs font-medium text-pink-600 text-left mb-2'>{errors.password?.message && (
+                <p className='text-sm font-medium text-pink-600 text-left mt-1 mb-2'>{errors.password?.message && (
     <span className='flex items-center'>
      <ErrorSharpIcon className='mr-1' sx={{ color: pink[500], fontSize: 16, verticalAlign: 'middle' }} />
      {errors.password.message}
     </span>
   )}</p>
+             <div className='text-md  absolute top-11 right-3'>
+               {
+                  (open === false)? <AiFillEyeInvisible style={{color: "rgb(90, 90, 90)"}} onClick={toggle} />:
+                  <AiFillEye style={{color: "rgb(90, 90, 90)"}} onClick={toggle} /> 
+               }
+             </div>
              </div>
              <div className='my-5'>
              <button type='submit'
