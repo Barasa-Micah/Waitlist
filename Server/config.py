339c81a8d49986529edbac493925cda479aad951
rhema254 <karhem254@gmail.com>
2024-07-16 22:49:11 +0300

    environment variables comments

--- Server/config.py
+++ Server/config.py
@@ -1,6 +1,8 @@
 from decouple import config
 from flask_sqlalchemy import SQLAlchemy
 
+# Create a .env file to store all your environment variables and configuration settings.
+
 
 class Config:
     SECRET_KEY = config('SECRET_KEY')
@@ -10,9 +12,9 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql://rhema:qwerty123@localhost/waitlist'
     DEBUG = True
     SQLALCHEMY_ECHO = True
-    MAIL_SERVER = config('MAIL_SERVER')
-    MAIL_USERNAME = config('MAIL_USERNAME')
-    MAIL_PASSWORD = config('MAIL_PASSWORD')
+    MAIL_SERVER = config('MAIL_SERVER') # Write this in your .env
+    MAIL_USERNAME = config('MAIL_USERNAME') # Write this in your .env
+    MAIL_PASSWORD = config('MAIL_PASSWORD') # Write this in your .env
     MAIL_PORT = 465
     MAIL_USE_SSL = True 
 
